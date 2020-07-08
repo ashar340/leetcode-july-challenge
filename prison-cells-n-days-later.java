0a68b20ae10fc52f08b46692ca8b62becdf819f9
Ananya <ananyas237@gmail.com>
2020-07-08 12:59:06 -0400

    added prison-cells-magic-number approach

---
+++ prison-cells-n-days-later.java
@@ -0,0 +1,66 @@
+/* 
+957. Prison Cells After N Days
+There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+Otherwise, it becomes vacant.
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+ 
+
+Example 1:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+Example 2:
+
+Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+Output: [0,0,1,1,1,1,1,0]
+ 
+
+Note:
+
+cells.length == 8
+cells[i] is in {0, 1}
+1 <= N <= 10^9 
+*/
+
+/*
+Here, the magic number in similarity to the game of life is 14, things repeat from then
+    N % 14 --> Pattern on after N days
+*/
+class Solution {
+    public int[] prisonNDaysLater_magicNumber(int[] cells, int N) {
+        if (N > 14) {
+            N = (N % 14) + 14;
+        } else {
+            N = N % 14;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int[] temp = new int[cells.length];
+            for (int j = 1; j < 7; j++) {
+                temp[j] = cells[j - 1] == cells[j + 1] ? 1 : 0;
+            }
+            cells = temp;
+        }
+        return cells;
+    }
+}
